ca018fbcd6cc2b1551f8be2cb24684cc3b86c30b
CRobertHyman <CRobertHyman@users.noreply.github.com>
2020-09-16 17:05:22 -0400

    Stable working uploaded

---
+++ BakedLightingRetry/Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=croberthyman@gmail.com&user[password]=Icw2batwIgu!AS" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ BakedLightingRetry/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@E:\Unity Class\Class 2\BakedLighting\BakedLighting\BakedLightingRetry\template.zip" -F "space_template[game_engine_version]=20192" https://account.altvr.com/api/space_templates/1563320244305396290.json
